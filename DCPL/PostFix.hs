b567a930021707df02aec1419ee024811b4aae18
Jose Antonio Ortega Ruiz <jao@gnu.org>
2010-10-03 17:48:52 +0200

    Initial commit

---
+++ DCPL/PostFix.hs
@@ -0,0 +1,19 @@
+------------------------------------------------------------------------------
+-- |
+-- Module: DCPL.PostFix
+-- Copyright: (c) 2010 Jose Antonio Ortega Ruiz
+-- License: BSD3-style (see LICENSE)
+--
+-- Maintainer: jao@gnu.org
+-- Stability: unstable
+-- Portability: portable
+-- Created: Sun Oct 03, 2010 17:45
+--
+--
+-- PostFix language (Chapter 1)
+--
+------------------------------------------------------------------------------
+
+
+module DCPL.PostFix where
+
